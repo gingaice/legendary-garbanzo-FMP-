4377423058c99bf86ad6a9457bfcd0334c1509c9
JonesCGD <80253281+gingaice@users.noreply.github.com>
2024-03-18 14:16:42 +0000

    player shoot fixed

--- Assets/Script/Player/PlayerShoot.cs
+++ Assets/Script/Player/PlayerShoot.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerShoot : MonoBehaviour
+public class PlayerShoot : NetworkBehaviour
 {
     [SerializeField]
     public GameObject Gun;
@@ -15,12 +16,29 @@ public class PlayerShoot : MonoBehaviour
 
     private void Update()
     {
+        if(!IsOwner) return;
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             if(ammo > 0)
             {
+                RequestFireServerRpc();
                 Fire();
             }
+        }
+    }
+
+    [ServerRpc]
+    void RequestFireServerRpc()
+    {
+        FireClientRpc();
+    }
+
+    [ClientRpc]
+    void FireClientRpc() //this is client side as it is the player telling the server that a bullet has been fired from this owner, it will then spawn the proj in on each game saving sending over more data
+    {
+        if (!IsOwner)
+        {
+            Fire();
         }
     }
     public void Fire()
--- Assets/Script/SecondTry/CreatingOwnClient/PlayerNetwork.cs
+++ Assets/Script/SecondTry/CreatingOwnClient/PlayerNetwork.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class PlayerNetwork : NetworkBehaviour
 {
@@ -10,29 +9,91 @@ public class PlayerNetwork : NetworkBehaviour
     //private NetworkVariable<Quaternion> _netRot = new NetworkVariable<Quaternion>(writePerm: NetworkVariableWritePermission.Owner);
     private Vector3 _vel;
     private float _rotVel;
+
     [SerializeField]
     private float _interpolationSpeed = 0.1f;
+    [SerializeField]
+    private bool _serverAuth;
+
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+
+        var permission = _serverAuth ? NetworkVariableWritePermission.Server : NetworkVariableWritePermission.Owner; // if serverauth is true then only the server can change _netstate stuff
+        _netstate = new NetworkVariable<PlayerNetworkData> (writePerm: permission);
+    }
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            //Destroy(transform.GetComponent<PlayerMovement>()); //this was bringing up errors as it was probably still trying to talk to the server in some way?
+            transform.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
     void Update()
     {
         if (IsOwner)
-        { //this is for sending
-            _netstate.Value = new PlayerNetworkData()
-            {
-                Pos = transform.position,
-                Rot = transform.rotation.eulerAngles
-            };
+        {
+            TransmitState();
         }
         else
-        { //this is for reading // adding some interpolation to make the characters move smoother on the other instance
-            transform.position = Vector3.SmoothDamp(transform.position, _netstate.Value.Pos, ref _vel, _interpolationSpeed);
-            transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _netstate.Value.Rot.y, ref _rotVel, _interpolationSpeed),0);
+        {
+            ConsumeState();
+        }
+
+        //if (IsOwner)
+        //{ //this is for sending
+        //    _netstate.Value = new PlayerNetworkData()
+        //    {
+        //        Pos = _rigidBody.position,
+        //        Rot = transform.rotation.eulerAngles
+        //    };
+        //}
+        //else
+        //{ //this is for reading // adding some interpolation to make the characters move smoother on the other instance //do a better interpolation later pls future me
+        //    _rigidBody.MovePosition(Vector3.SmoothDamp(transform.position, _netstate.Value.Pos, ref _vel, _interpolationSpeed));
+        //    transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _netstate.Value.Rot.y, ref _rotVel, _interpolationSpeed),0);
+        //}
+    }
+
+    private void TransmitState() //to control sending the players data to the server
+    {
+        var state = new PlayerNetworkData
+        {
+            Pos = _rigidBody.position,
+            Rot = transform.rotation.eulerAngles
+        };
+
+        if(IsServer || !_serverAuth)
+        {
+            _netstate.Value = state;
+        }
+        else
+        {
+            TransmitStateServerRpc(state);
         }
     }
+
+    [ServerRpc]
+    private void TransmitStateServerRpc(PlayerNetworkData state)
+    {
+        _netstate.Value = state;
+    }
+
+
+    private void ConsumeState() //to recieve the data sent from the server
+    {
+        _rigidBody.MovePosition(Vector3.SmoothDamp(transform.position, _netstate.Value.Pos, ref _vel, _interpolationSpeed));
+        transform.rotation = Quaternion.Euler(0, Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _netstate.Value.Rot.y, ref _rotVel, _interpolationSpeed),0);
+    }
+
 
     struct PlayerNetworkData : INetworkSerializable //this is to speed up the data being sent over the internet to stop it being choppy and is supported by the networkserialize below
     {
         private float _X, _Z;
-        private float _YRot;
+        private short _YRot; //short is used to send less bytes over the web although a small amount as a short is equal too 2 bytes
 
 
         internal Vector3 Pos //internal is used when the designer dont want to show the variable everywhere in public but it still allows access outside of the function in the code.
@@ -48,11 +109,11 @@ public class PlayerNetwork : NetworkBehaviour
         
         internal Vector3 Rot
         {
-            get => new Vector3 (0, _YRot, 0);
+            get => new (0, _YRot, 0);
 
             set
             {
-                _YRot = value.y;
+                _YRot = (short)value.y;
             }
         }
 

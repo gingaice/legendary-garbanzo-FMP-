691742cc5ceccf7c2b62cf5553ea66e7b599c3c0
JonesCGD <80253281+gingaice@users.noreply.github.com>
2024-04-11 20:44:57 +0100

    half way through i leave it for later i eat

--- Assets/Script/Misc/CreatingLobby/LobbyTest.cs
+++ Assets/Script/Misc/CreatingLobby/LobbyTest.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
-using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class LobbyTest : MonoBehaviour
 {
+    private Lobby hostLobby;
+
+    private string playerName;
+
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -19,6 +22,8 @@ public class LobbyTest : MonoBehaviour
         };
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        playerName = "bong" + Random.Range(1, 36);
+        Debug.Log("Id is: " + playerName);
     }
 
     private async void CreateLobby()
@@ -27,11 +32,101 @@ public class LobbyTest : MonoBehaviour
         {
             string lobbyName = "bingbong";
             int maxPlayers = 5;
-            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions 
+            {
+                IsPrivate = true,
+                Player = new Player
+                {
+                    Data = new Dictionary<string, PlayerDataObject>
+                { {"PlayerName" ,new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, playerName) } }
+                },
+            };
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, createLobbyOptions);
+
+            hostLobby = lobby;
+
+            StartCoroutine(HeartBeatLobbyCoroutine(lobby.Id, 15));
+
+            PrintPlayers(hostLobby);
         }
         catch (LobbyServiceException ex)
         {
             Debug.Log(ex);
+        }
+    }
+
+    private IEnumerator HeartBeatLobbyCoroutine(string lobbyId, float waitTimeSeconds)
+    {
+        var delay = new WaitForSecondsRealtime(waitTimeSeconds);
+        while (true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
+    }
+
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter> {
+                new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0" , QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            Debug.Log("lobbies found " + queryResponse.Results.Count);
+            foreach(Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        catch(LobbyServiceException ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+
+    private async void joinLobbyByCode(string lobbyCode)
+    {
+        try
+        {
+            await Lobbies.Instance.JoinLobbyByIdAsync(lobbyCode);
+
+        }
+        catch (LobbyServiceException ex)
+        {
+            Debug.Log(ex);
+        }
+
+    }    
+    
+    private async void quickJoinLobby()
+    {
+        try
+        {
+            await Lobbies.Instance.QuickJoinLobbyAsync();
+
+        }
+        catch (LobbyServiceException ex)
+        {
+            Debug.Log(ex);
+        }
+
+    }
+
+    private void PrintPlayers(Lobby lobby)
+    {
+        foreach(Player player in lobby.Players)
+        {
+            Debug.Log(player.Id + " " + player.Data["PlayerName"].Value);
         }
     }
 }
--- Assets/Script/Player/PlayerFOVEditor.cs
+++ Assets/Script/Player/PlayerFOVEditor.cs
@@ -1,27 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(PlayerFOV))]
-public class PlayerFOVEditor : Editor
-{
-	void OnSceneGUI()
-    {
-        PlayerFOV fow = (PlayerFOV)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
-        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+//[CustomEditor(typeof(PlayerFOV))]
+//public class PlayerFOVEditor : Editor
+//{
+//	void OnSceneGUI()
+//    {
+//        PlayerFOV fow = (PlayerFOV)target;
+//        Handles.color = Color.white;
+//        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+//        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+//        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
 
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
-        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+//        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+//        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
-        Handles.color = Color.red;
-        foreach (Transform visibleTarget in fow.visibleTargets)
-        {
-            Handles.DrawLine(fow.transform.position, visibleTarget.position);
-        }
-    }
-}
+//        Handles.color = Color.red;
+//        foreach (Transform visibleTarget in fow.visibleTargets)
+//        {
+//            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+//        }
+//    }
+//}
 
